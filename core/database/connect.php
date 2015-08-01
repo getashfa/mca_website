d9e2fec1066a9b16c9af781b11c1fe34d0652838
getashfa <getashfa@gmail.com>
2015-08-01 15:05:40 +0530

    removed dbconnect file

--- core/database/connect.php
+++
@@ -1,5 +0,0 @@
-<?php
-$connect_error = "<h2>sorry the server is under maintenence, please visit after some time</h2>";	//this is error message
-mysql_connect('localhost', 'root', 'straifo') or die($connect_error);
-mysql_select_db('mca') or die($connect_error);
-?>
